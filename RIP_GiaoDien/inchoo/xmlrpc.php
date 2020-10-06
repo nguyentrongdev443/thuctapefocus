72cb8b8b20756d7954ed2c17902ab4bafb80cd6b
phanvantan645 <phanvantan645@gmail.com>
2020-10-06 16:54:01 +0700

    Giao dien trang chu

---
+++ RIP_GiaoDien/inchoo/xmlrpc.php
@@ -0,0 +1,6 @@
+
+<!-- saved from url=(0029)inchoo/xmlrpc.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"></head><body><h1>403 Forbidden</h1>
+Request forbidden by administrative rules.
+
+</body></html>
